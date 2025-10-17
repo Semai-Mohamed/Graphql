ea5696249fc33d3d744bf5d8069ed4cdc2c61de9
Semai-Mohamed <semaimohamed860@gmail.com>
2025-10-17 20:04:26 +0200

    graphql-start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GraphQLSchemaHost } from '@nestjs/graphql';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const {schema} = app.get(GraphQLSchemaHost)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
